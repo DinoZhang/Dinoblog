1aa3d5f2b3b759e12d5674d457a488fe1019e490
Dinozhang <victordinozhang@gmail.com>
2014-08-29 18:02:00 +0800

    statistics

--- jae/themes/dinothemv1.5/index.php
+++ jae/themes/dinothemv1.5/index.php
@@ -1,5 +1,8 @@
 <?php get_header(); ?>
-
+<script type="text/javascript">
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F929d39cf5136f9b0b3fa586cbadf7d59' type='text/javascript'%3E%3C/script%3E"));
+</script>
 <div class="bodybox">
   <div class="main">
     <div class="common">
