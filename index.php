22eb19720ece9b64d2c14bdd243de89e3d318b76
53156919_m <unknown>
2014-08-30 14:01:46 +0800

    取消多余的js

--- index.php
+++ index.php
@@ -1,7 +1,4 @@
-<script type="text/javascript">
-var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
-document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F929d39cf5136f9b0b3fa586cbadf7d59' type='text/javascript'%3E%3C/script%3E"));
-</script>
+
 <?php
 /**
  * Front to the WordPress application. This file doesn't do anything, but loads
