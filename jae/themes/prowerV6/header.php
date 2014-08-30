23deb6fdec769a2ee9325b5ba840a8c7721f16cf
53156919_m <unknown>
2014-08-31 02:04:11 +0800

    取消在head中统计

--- jae/themes/prowerV6/header.php
+++ jae/themes/prowerV6/header.php
@@ -23,10 +23,7 @@
 		wp_enqueue_script( 'comment-reply' );
 		wp_head();
 	?>
-	<script type="text/javascript">
-var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
-document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F929d39cf5136f9b0b3fa586cbadf7d59' type='text/javascript'%3E%3C/script%3E"));
-</script>
+	
 </head>
 
 <body>
