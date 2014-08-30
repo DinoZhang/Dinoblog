bf230ef6c5c7ea984fec74a16dd44702f2fcbfb5
Dinozhang <victordinozhang@gmail.com>
2014-08-31 01:39:23 +0800

    添加统计

--- jae/themes/prowerV6/footer.php
+++ jae/themes/prowerV6/footer.php
@@ -1,7 +1,11 @@
 </div>
 <footer id="footer">
 
-	<p>&copy; 2013 <?php bloginfo('name'); ?>. Powered by <a href="http://wordpress.org/" target="_blank">WordPress</a>. Theme by <a href="http://dinoblog.jd-app.com/" target="_blank">Dino</a>.</p>
+	<p>&copy; 2014 <?php bloginfo('name'); ?>. Powered by <a href="http://wordpress.org/" target="_blank">WordPress</a>. Theme by <a href="http://dinoblog.jd-app.com/" target="_blank">Dino</a>.</p>
+	<script type="text/javascript">
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F929d39cf5136f9b0b3fa586cbadf7d59' type='text/javascript'%3E%3C/script%3E"));
+</script>
 </footer>
 <?php wp_footer(); ?>
 </body>
