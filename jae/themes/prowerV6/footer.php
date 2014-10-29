6717f25acdd8f2452e834ea0a8b9329051269bbb
53156919_m <unknown>
2014-10-29 18:12:09 +0800

    再次修改google统计

--- jae/themes/prowerV6/footer.php
+++ jae/themes/prowerV6/footer.php
@@ -6,7 +6,7 @@
 var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
 document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F929d39cf5136f9b0b3fa586cbadf7d59' type='text/javascript'%3E%3C/script%3E"));
 </script>
-  <script type=“text/javascript”>
+  <script type="text/javascript">
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
