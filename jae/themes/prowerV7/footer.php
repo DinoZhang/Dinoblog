dae0d79dbf48ffeda69e14afae1370f2fd4fcaf2
Dinozhang <victordinozhang@gmail.com>
2014-11-03 17:37:27 +0800

    prower
    Signed-off-by: Dinozhang <victordinozhang@gmail.com>

---
+++ jae/themes/prowerV7/footer.php
@@ -0,0 +1,10 @@
+</div>
+<footer id="footer">
+	<aside id="footer_box">
+		<div id="linkcat"><?php wp_list_bookmarks('categorize=0&title_li=0&before='); ?></div>
+	</aside>
+	<p>&copy; 2014 <?php bloginfo('name'); ?>. Powered by <a href="http://wordpress.org/" target="_blank">WordPress</a>. Theme by <a href="http://www.prower.cn/work/2569" target="_blank">porwer</a>&<a href="http://www.prower.cn/work/2569" target="_blank">Dino</a>.</p>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
