85dfcad8d864082ac84533218ba8d451d519bc72
53156919_m <unknown>
2014-11-04 12:36:03 +0800

    添加cnzz统计

--- jae/themes/prowerV6/footer.php
+++ jae/themes/prowerV6/footer.php
@@ -5,6 +5,8 @@
 	<script type="text/javascript">
 var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
 document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F929d39cf5136f9b0b3fa586cbadf7d59' type='text/javascript'%3E%3C/script%3E"));
+  </script>
+  <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1253519934'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "v1.cnzz.com/z_stat.php%3Fid%3D1253519934' type='text/javascript'%3E%3C/script%3E"));</script>
 
 </footer>
 <?php wp_footer(); ?>
