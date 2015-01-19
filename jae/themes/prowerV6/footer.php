7c64cd0e7261e2800957a15272ee0ec64c6f0671
53156919_m <unknown>
2015-01-19 22:24:54 +0800

    将cnzz统计更改为图片格式

--- jae/themes/prowerV6/footer.php
+++ jae/themes/prowerV6/footer.php
@@ -1,13 +1,14 @@
 </div>
 <footer id="footer">
 
-	<p>&copy; 2014-2015 <?php bloginfo('name'); ?>. Powered by <a href="http://wordpress.org/" target="_blank">WordPress</a>. Theme by Prower&<a href="http://dinoblog.jd-app.com/" target="_blank">Dino</a>.</p>
+	<p>&copy; 2014-2015 <?php bloginfo('name'); ?>. Powered by <a href="http://wordpress.org/" target="_blank">WordPress</a>. Theme by Prower&<a href="http://dinoblog.jd-app.com/" target="_blank">Dino</a>.
+     <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1253519934'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "v1.cnzz.com/z_stat.php%3Fid%3D1253519934%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script>
+    </p>
 	<script type="text/javascript">
 var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
 document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F929d39cf5136f9b0b3fa586cbadf7d59' type='text/javascript'%3E%3C/script%3E"));
   </script>
-  <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1253519934'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "v1.cnzz.com/z_stat.php%3Fid%3D1253519934' type='text/javascript'%3E%3C/script%3E"));</script>
-
+  
 </footer>
 <?php wp_footer(); ?>
 </body>
